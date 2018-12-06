965211ab113663c8e35c2ad62b794243e450626d
UnicornoftheSea99 <erhyu@smith.edu>
2018-12-06 15:43:58 -0500

    Things

--- src/main/java/edu/smith/cs/csc212/p5/PowerUps.java
+++ src/main/java/edu/smith/cs/csc212/p5/PowerUps.java
@@ -3,8 +3,10 @@ package edu.smith.cs.csc212.p5;
 //Imports
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class PowerUps extends WorldObject{
 
@@ -15,12 +17,13 @@ public class PowerUps extends WorldObject{
 	//Create Fish Food
 	@Override
 	public void draw(Graphics2D g) {
-		Shape food = new Ellipse2D.Double(0, 0, 0.25, 0.25);
-		g.setColor(Color.orange);
-		g.fill(food);
-
-
+		//Shape food = new Ellipse2D.Double(0, 0, 0.25, 0.25);
+		Shape powerup = new Rectangle2D.Double(0.75, 0.75, 0.5, 0.5);
+		g.setColor(Color.YELLOW);
+		g.fill(powerup);
+	
 	}
+	
 
 	//Fish food doesn't move
 	@Override
